4f6105a08f6e624399af187404892ed65db52c2a
Forkhammer <forkhammer@gmail.com>
2017-09-13 19:59:56 +0500

    urlencode

--- pycdek/client.py
+++ pycdek/client.py
@@ -2,9 +2,8 @@
 import json
 import hashlib
 import datetime
-from urllib.request import urlopen, Request, HTTPError
+from urllib.request import urlopen, Request, HTTPError, urlencode
 from io import StringIO
-from urllib import urlencode
 from xml.etree import ElementTree
 from abc import ABCMeta, abstractmethod
 
