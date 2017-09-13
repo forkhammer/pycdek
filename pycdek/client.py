d23d773d974673fbfbe83b7e385780bdb99a1479
Forkhammer <forkhammer@gmail.com>
2017-09-13 21:11:53 +0500

    post

--- pycdek/client.py
+++ pycdek/client.py
@@ -170,7 +170,7 @@ class Client(object):
             'goods': goods,
         }
 
-        return json.loads(cls._exec_request(cls.CALCULATOR_URL, json.dumps(params), 'POST'))
+        return json.loads(cls._exec_request(cls.CALCULATOR_URL, params, 'POST'))
 
     @classmethod
     def get_delivery_points(cls, city_id=None):
